9456b59bdb33846e08a1461e2499ff64d4f31060
chuyan <379482455@qq.com>
2023-09-06 00:18:03 +0800

    创建玩家的有限状态机

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -3,119 +3,28 @@ using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
 
-public class Player : Entity
+public class Player : MonoBehaviour
 {
-    private float xInput;
+    public PlayerStateMachine stateMachine { get; private set; }
 
-    [Header("Move Info")]
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float jumpForce;
+    public PlayerIdleState idleState { get; private set; }
+    public PlayerMoveState moveState { get; private set; }
 
-    [Header("Dash Info")]
-    [SerializeField] private float dashSpeed;
-    [SerializeField] private float dashDuration;
-    private float dashTime;
-    [SerializeField] private float dashCooldown;
-    private float dashCooldownTimer;
-
-    [Header("Attack Info")]
-    [SerializeField] private float comboTime;
-    private float comboTimeWindow;
-    private bool isAttaching;
-    private int comboCounter;
-
-    protected override void Start()
+    private void Awake()
     {
-        base.Start();
+        stateMachine = new PlayerStateMachine();
+
+        idleState = new PlayerIdleState(this, stateMachine, "Idle");
+        moveState = new PlayerMoveState(this, stateMachine, "Move");
     }
 
-    protected override void Update()
+    private void Start()
     {
-        base.Update();
-
-        CheckInput();
-        Movement();
-        FlipCotroller();
-
-        if (dashCooldownTimer >= 0) dashCooldownTimer -= Time.deltaTime;
-        if (dashTime >= 0) dashTime -= Time.deltaTime;
-        if(comboTimeWindow >= 0) comboTimeWindow -= Time.deltaTime;
-
-        AnimatorContollers();
+        stateMachine.Initialize(idleState);
     }
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////
-    public void AttachOver()
+    private void Update()
     {
-        isAttaching = false;
-        ++comboCounter;
-        if(comboCounter > 2) comboCounter = 0;
-    } 
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////
-    private void Movement()
-    {
-        if (isAttaching)
-            rb.velocity = new Vector2(0, 0);
-        else if (dashTime > 0)
-            rb.velocity = new Vector2(facingDir * dashSpeed, 0);
-        else
-            rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
-    }
-
-    private void CheckInput()
-    {
-        xInput = Input.GetAxis("Horizontal");
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-            StartAttachEvent();
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-            DashAbility();
-
-        if (Input.GetKeyDown(KeyCode.Space) 
-            && isGrounded) Jump();
-    }
-
-    private void StartAttachEvent()
-    {
-        if (!isGrounded) return;
-
-        if (comboTimeWindow < 0) comboCounter = 0;
-
-        isAttaching = true;
-        comboTimeWindow = comboTime;
-    }
-
-    private void DashAbility()
-    {
-        if (dashCooldownTimer < 0
-            && !isAttaching)
-        {
-            dashTime = dashDuration;
-            dashCooldownTimer = dashCooldown;
-        }
-    }
-
-    private void Jump()
-    {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-    }
-
-    private void FlipCotroller()
-    {
-        if ((rb.velocityX > 0 && !isFaceingRight)
-            || (rb.velocityX < 0 && isFaceingRight))
-            Flip();
-    }
-    
-    private void AnimatorContollers()
-    {
-        anim.SetFloat("yVelocity", rb.velocity.y);
-        anim.SetBool("isMoving", rb.velocity.x != 0);
-        anim.SetBool("isGrounded", isGrounded);
-        anim.SetBool("isDashing", dashTime > 0);
-        anim.SetBool("isAttaching", isAttaching);
-        anim.SetInteger("comboCounter", comboCounter);
+        stateMachine.currentState.Update();
     }
 }
